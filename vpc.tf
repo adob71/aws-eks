4b2fcffedc8db2525e0f2dca411d192b6905fabc
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-05-16 06:07:25 +0100

    commit

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default     = "us-east-2"
+  default     = "eu-west-2"
   description = "AWS region"
 }
 
@@ -25,8 +25,8 @@ module "vpc" {
   name                 = "education-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["10.0.1.0/24",  "10.0.2.0/24",  "10.0.3.0/24",  "10.0.4.0/24"]
+  public_subnets       = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24", "10.0.14.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true

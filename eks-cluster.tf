901fc7492d757676b7078b36591a9db8fbccb229
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-06-07 07:18:11 +0100

    commit

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -18,14 +18,14 @@ module "eks" {
       name                          = "worker-group-1"
       instance_type                 = "t2.small"
       #      instance_type                 = "t4g.small"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      #      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
     },
     {
       name                          = "worker-group-2"
       instance_type                 = "t2.small"
       #      instance_type                 = "t4g.small"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      #      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 2
     },
   ]

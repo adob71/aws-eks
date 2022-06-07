81609caa9cc4a1b54cf06c229c1ae733a1ffd607
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-06-07 05:30:25 +0100

    commit

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = ">= 17.24.0"
+  version         = "17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.20"
 
--- vpc.tf
+++ vpc.tf
@@ -26,8 +26,10 @@ module "vpc" {
   name                 = "education-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24",  "10.0.2.0/24",  "10.0.3.0/24",  "10.0.4.0/24"]
-  public_subnets       = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24", "10.0.14.0/24"]
+
+  public_subnets       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets      = ["10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
+
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true

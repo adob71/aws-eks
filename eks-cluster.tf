1bb0a50c939e9976f53c36a84ffc742b2d7958ae
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-06-07 05:25:49 +0100

    commit

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,7 +3,10 @@ module "eks" {
   version         = ">= 17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.20"
-  worker_ami_name_filter = "amazon-eks-arm64-node-1.20-v20210716"
+
+  worker_ami_name_filter = "amazon-eks-node-1.20-v20210716"
+  #  worker_ami_name_filter = "amazon-eks-arm64-node-1.20-v20210716"
+
   subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
@@ -15,17 +18,21 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      #instance_type                 = "t2.small"
-      instance_type                 = "t4g.small"
-      additional_userdata           = "echo foo bar"
+
+      instance_type                 = "t2.small"
+      #      instance_type                 = "t4g.small"
+
+      #      additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
     },
     {
       name                          = "worker-group-2"
-      #instance_type                 = "t2.small"
-      instance_type                 = "t4g.small"
-      additional_userdata           = "echo foo bar"
+
+      instance_type                 = "t2.small"
+      #      instance_type                 = "t4g.small"
+
+      #      additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 2
     },
